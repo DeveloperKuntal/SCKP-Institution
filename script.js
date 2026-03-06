1713c47c0718ef95796f93dbdfd7eba95021baa1
DeveloperKuntal <kuntalmishra03@gmail.com>
2026-03-06 08:39:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 window.onload = function(){
+
 document.getElementById("popupOverlay").style.display="flex";
+
 }
 
 function minimizeForm(){
+
 document.getElementById("popupOverlay").style.display="none";
 document.getElementById("floatingBtn").style.display="block";
+
 }
 
 function openForm(){
+
 document.getElementById("popupOverlay").style.display="flex";
 document.getElementById("floatingBtn").style.display="none";
+
 }
